3121cf5802e3d4e75c106f0aa90e49d257be8510
MateoMalloHuergo <167899090+MateoMalloHuergo@users.noreply.github.com>
2024-07-02 09:54:27 -0300

    Initial commit

---
+++ Assets/MostrarObjetos.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MostrarObjetos : MonoBehaviour
+{
+    public GameObject[] objetos;
+    int obj = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            objetos[obj].SetActive(false);
+            obj--;
+            if (obj < 0)
+            {
+                obj = 24;
+            }
+            objetos[obj].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            objetos[obj].SetActive(false);
+            obj++;
+            if (obj > 24)
+            {
+                obj = 0;
+            }
+            objetos[obj].SetActive(true);
+        }
+    }
+    
+}
